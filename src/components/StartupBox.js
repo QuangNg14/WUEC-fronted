f7dc253a35f43463b91382dfb4e8a726a29dc0dc
Minh Anh Ngo <anhmngo@huntsman-ve703-01754.apn.wlan.private.upenn.edu>
2022-03-26 15:51:43 -0400

    added modal

--- src/components/StartupBox.js
+++ src/components/StartupBox.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StartupBox.css";
 import { FaComment } from "react-icons/fa";
 import { BsCaretUpSquare } from "react-icons/bs";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
 const StartupBox = (props) => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
+  const closeModal = () => {
+    setOpen(false)
+
+  }
   return (
     <div className="box-container">
-      <div className="image">
+
+      <div className="image" onClick={() => setOpen(true)}>
         <img src={props.image} className="img" />
       </div>
       <div className="content">
+        <Modal
+          open={open}
+          onClose={closeModal}
+          className={classes.paper}
+          style={{ 'maxHeight': '1000px', 'overflowY': 'scroll', position: "absolute" }}
+        >
+          <div>
+            hello
+          </div>
+
+        </Modal>
         <div className="name">{props.name}</div>
         <div className="description">{props.description}</div>
         <div className="info">
@@ -25,7 +47,24 @@ const StartupBox = (props) => {
         </div>
       </div>
     </div>
+
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginLeft: "20%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "60%",
+    height: "auto",
+    backgroundColor: "black",
+    overflowY: "scroll"
+
+  },
+  
+}));
+
 
 export default StartupBox;
