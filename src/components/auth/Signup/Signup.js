4d59f35f42f13170d1e99f4402fd9afa41faa2bc
QuangNg14 <nnquang.1412@gmail.com>
2022-04-10 13:44:51 -0500

    .

--- src/components/auth/Signup/Signup.js
+++ src/components/auth/Signup/Signup.js
@@ -55,11 +55,11 @@ const Signup = () => {
     <>
       <div className="bgImage"></div>
       <div className="headerAuth">
-        <img
+        {/* <img
           className="logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
           alt="Netflix Logo"
-        />
+        /> */}
       </div>
       <div className="wrap">
         <div className="title">Sign Up</div>
